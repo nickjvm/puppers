ca840e154beb0cfe842648ea974369301e354a0e
Nick VanMeter <nickjvm@gmail.com>
2018-06-19 08:58:54 -0600

    setup server and client fetch POC

---
+++ server.js
@@ -0,0 +1,41 @@
+const bodyParser = require('body-parser')
+const path = require('path');
+const http = require('http');
+
+const express = require('express');
+const dotenv = require('dotenv')
+
+dotenv.config({path: path.join(__dirname, `.env.${process.env.NODE_ENV || 'development'}.local`)})
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/pets', (req, res) => {
+  if (process.env.NODE_ENV === 'development') {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  }
+
+  return http.get(`http://api.petfinder.com/pet.find?key=${process.env.PETFINDER_API_KEY}&location=80526&format=json&animal=dog`, response => {
+    let rawData = '';
+    response.on('data', (chunk) => {
+      rawData += chunk;
+    });
+    response.on('end', () => {
+      try {
+        const parsedData = JSON.parse(rawData);
+        res.send(parsedData.petfinder);
+      } catch (e) {
+        console.error(e.message);
+      }
+    });
+  })
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, console.log(`App running on port ${PORT}`));
--- src/App.js
+++ src/App.js
@@ -3,15 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    pet: {
+      name: {}
+    },
+    photos: {}
+  }
+
+  async componentDidMount() {
+    const request = await fetch('http://localhost:8080/pets');
+    const response = await request.json()
+    const pet = response.pets.pet[0];
+    const photos = pet.media.photos.photo.reduce((acc, arr) => {
+      return Object.assign(acc, { [arr['@size']]: arr['$t'] })
+    }, {})
+    this.setState({ pet, photos })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1>{this.state.pet.name['$t']}</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <img src={this.state.photos.x} alt={this.state.pet.name['$t']}/>
         </p>
       </div>
     );
