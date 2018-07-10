1e7c15156d385aa0c954b1b0edfb726cd77f9b33
Nick VanMeter <nickjvm@gmail.com>
2018-07-10 08:01:03 -0600

    add env config to server.js

--- server.js
+++ server.js
@@ -3,17 +3,20 @@ const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
 const http = require('http');
+const dotenv = require('dotenv');
 var cors = require('cors')
 
 app.use(cors())
 app.use(express.static(path.join(__dirname, 'build')));
+
+dotenv.config({path: path.join(__dirname, `.env.${process.env.NODE_ENV || 'development'}.local`)})
 
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
 
 app.get('/pets', function (req, res) {
- return http.get('http://api.petfinder.com/pet.find?format=json&location=80525&animal=dog&key=76f5d5109ce0fff9b790293c8e5b121c', response => {
+ return http.get(`http://api.petfinder.com/pet.find?format=json&location=80525&animal=dog&key=${process.env.PETFINDER_API_KEY}`, response => {
      let rawData = ''
      response.on('data', (chunk) => {
          rawData += chunk;
@@ -25,16 +28,14 @@ app.get('/pets', function (req, res) {
          res.send(parsedData.petfinder);
          } catch(e) {
              console.log(e.message);
-             
+
          }
      })
  });
 });
 
-
-
-// app.get('/', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(process.env.PORT || 8080, () => {console.log("Success!!")});
