178b3428de0903c3fd401e59aee933255b0c802b
Nick VanMeter <nickjvm@gmail.com>
2018-08-01 13:05:28 -0600

    add geolocation lookup

--- server.js
+++ server.js
@@ -1,41 +1,68 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const path = require('path');
+const http = require("http");
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+var Geocodio = require("geocodio");
+
+dotenv.config({
+  path: path.join(
+    __dirname,
+    `.env.${process.env.NODE_ENV || "development"}.local`
+  )
+});
+
 const app = express();
-const http = require('http');
-const dotenv = require('dotenv');
-var cors = require('cors')
 
-app.use(cors())
-app.use(express.static(path.join(__dirname, 'build')));
+var geocodio = new Geocodio({ api_key: process.env.GEOCODIO_API_KEY });
 
-dotenv.config({path: path.join(__dirname, `.env.${process.env.NODE_ENV || 'development'}.local`)})
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
+app.get("/ping", function(req, res) {
+  return res.send("pong");
 });
 
-app.get('/pets', function (req, res) {
- return http.get(`http://api.petfinder.com/pet.find?format=json&location=80525&animal=dog&key=${process.env.PETFINDER_API_KEY}`, response => {
-     let rawData = ''
-     response.on('data', (chunk) => {
-         rawData += chunk;
-     });
-     response.on('end', () =>{
-         try {
-         const parsedData = JSON.parse(rawData)
-         console.log(parsedData);
-         res.send(parsedData.petfinder);
-         } catch(e) {
-             console.log(e.message);
-
-         }
-     })
- });
+app.post("/geo", function(req, res) {
+  geocodio.get("reverse", { q: `${req.body.lat},${req.body.long}` }, function(
+    err,
+    response
+  ) {
+    if (err) throw err;
+    return res.json({
+      zip: JSON.parse(response).results[0].address_components.zip
+    });
+  });
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/pets", function(req, res) {
+  return http.get(
+    `http://api.petfinder.com/pet.find?count=24&format=json&location=${
+      req.query.location
+    }&animal=dog&key=${process.env.PETFINDER_API_KEY}`,
+    response => {
+      let rawData = "";
+      response.on("data", chunk => {
+        rawData += chunk;
+      });
+      response.on("end", () => {
+        try {
+          const parsedData = JSON.parse(rawData);
+          res.send(parsedData.petfinder);
+        } catch (e) {
+          console.log(e.message);
+        }
+      });
+    }
+  );
 });
 
-app.listen(process.env.PORT || 8080, () => {console.log("Success!!")});
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}!`);
+});
